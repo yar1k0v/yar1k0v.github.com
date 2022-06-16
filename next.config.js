01add3d85c5891853eaf0fb316ce27779e2880b9
Yaroslav Kovalov <yaroslav@thea-auto.com>
2022-06-16 10:02:33 +0600

    fixed export

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/yar1k0v.github.io/' : '',
+  assetPrefix: './',
+  // assetPrefix: isProd ? '/yar1k0v.github.io/' : '',
   images: {
     loader: 'akamai',
     path: '',
